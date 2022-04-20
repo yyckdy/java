29bb1afc72c2772ef8afec29d4030b706cf6e337
yyckdy <48600672@qq.com>
2022-04-20 17:03:06 +0800

    1

--- java提高/logback-app/src/logback/Test.java
+++ java提高/logback-app/src/logback/Test.java
@@ -1,10 +1,29 @@
 package logback;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 快速搭建logback日志框架记录程序执行情况 到文件中
  */
 public class Test {
-    public static void main(String[] args) {
+    //创建logback的日志对象
+    public static final Logger LOGGER = LoggerFactory.getLogger("Test.class");
 
+    public static void main(String[] args) {
+        while (true) {
+            try {
+                LOGGER.debug("main方法开始执行");
+                LOGGER.info("开始记录第二行日志，开始做除法");
+                int a = 10;
+                int b = 0;
+                LOGGER.trace("a=" + a);
+                LOGGER.trace("b=" + b);
+                System.out.println(a / b);
+            } catch (Exception e) {
+                e.printStackTrace();
+                LOGGER.error("功能出现异常，"+e);
+            }
+        }
     }
 }
